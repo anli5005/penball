39b913203ba1c7bd7bfeb7e0ff46a119f05a3670
Anthony Li <me@anli.dev>
2021-04-15 23:04:20 -0400

    Add Xcode stuff

---
+++ Penball/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Penball
+//
+//  Created by Anthony Li on 4/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Penball/PenballApp.swift
@@ -0,0 +1,17 @@
+//
+//  PenballApp.swift
+//  Penball
+//
+//  Created by Anthony Li on 4/15/21.
+//
+
+import SwiftUI
+
+@main
+struct PenballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
